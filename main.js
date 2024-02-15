fb7dae34d641f54fe1cbe4a27edd6dce93371f8a
Rishabh <rishabhshah203@gmail.com>
2024-02-15 23:47:54 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    var typed = new Typed(".home .home-content h3 .text", {
+        strings: ["Developer.", "Tech Enthusiast.", "Problem Solver.", 
+          "Innovator.", "Continuous Learner.", "Team Collaborator."],
+        typeSpeed:100,
+        backSpeed:100,
+        backDelay:1000,
+        startDelay:1000,
+        loop:true
+    });
+}
